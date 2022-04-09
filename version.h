11107e21c49405aad5694e8247e7084b9b3b3000
peteGSX <peteracole@outlook.com.au>
2022-04-09 19:47:33 +1000

    Updated version and config example

--- version.h
+++ version.h
@@ -1,8 +1,12 @@
 #ifndef version_h
 #define version_h
 
-#define VERSION "0.0.2"
+#define VERSION "0.0.3"
 
+// 0.0.3 includes:
+//  - Bug fix for two homing issues and cleaner debug output.
+//  - Also removed ability for users to enable/disable relay outputs to keep things simple.
+//    If users don't wish to use the relay outputs, they can simply leave them unused.
 // 0.0.2 includes:
 //  - Steps sent by CommandStation SERVO command, no local position/step configuration.
 //  - Phase switch also by SERVO command.
